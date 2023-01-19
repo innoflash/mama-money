af33c89ee372720531b787cb16294c09c75982d2
innoflash <innocentmazando@gmail.com>
2023-01-19 19:14:30 +0200

    added lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+    private readonly destroy$: Subject<void> = new Subject<void>();
+
+    public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+    }
 }
