e865be9fc8183a66ee763b6189300c9c2e137b7a
innoflash <innocentmazando@gmail.com>
2023-01-20 18:54:39 +0200

    trying https

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,7 +1,7 @@
 export const baseEnv = {
     openWeatherAPIKey: '4ca0db2e17dce86fea6dee6ef63b13c6',
     apiUrls: {
-        geocoding: 'http://api.openweathermap.org/geo/1.0',
+        geocoding: 'https://api.openweathermap.org/geo/1.0',
         weather: 'https://api.openweathermap.org/data/3.0'
     }
 }
